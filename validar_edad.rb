97d2f0fe85dfaa6de114847ed803165edd52afd1
Sebastián Andrés Tapia Oviedo <sebatapiaoviedo@gmail.com>
2020-07-13 00:35:46 -0400

    desafio_validaredad

---
+++ validar_edad.rb
@@ -0,0 +1,30 @@
+edad = ARGV[0].to_i
+if edad >= 18
+    puts "es mayor"
+else
+    puts "es menor"
+end
+
+computador = Random.rand(0..120)
+print "#{computador} "
+if computador >= 18
+    puts "es mayor"
+else
+    puts "es menor"
+end
+
+computador = Random.rand(0..120)
+print "#{computador} "
+if computador >= 18
+    puts "es mayor"
+else
+    puts "es menor"
+end
+
+computador = Random.rand(0..120)
+print "#{computador} "
+if computador >= 18
+    puts "es mayor"
+else
+    puts "es menor"
+end
